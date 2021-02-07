4480118509d67d0fe5cb6176514354840462f4c0
Khuzaima Arham <khuzaima8042@gmail.com>
2021-02-07 21:29:57 +0500

    initial commit

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+/*feature detection using modernizr
+
+function UploadSupported(){
+    if(navigator.userAgent.match(/(Android(1.0|1.1|1.5|1.6|2.0|2.1))|(Windows Phone(OS 7|8.0))|(XBLWP)|(ZuneWP)|(w(eb)?OSBrowser)|(webOS)|()
+    ))
+}*/
+var b = "KA";
+console.log(b);
+/*Selecting a file*/
+/* Reading the file*/
