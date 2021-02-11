a881975a40b8d85ec5fbb287f8a07621bf0982ef
Khuzaima Arham <khuzaima8042@gmail.com>
2021-02-11 20:42:49 +0500

    Updated UI

--- JS/main.js
+++ JS/main.js
@@ -4,7 +4,5 @@ function UploadSupported(){
     if(navigator.userAgent.match(/(Android(1.0|1.1|1.5|1.6|2.0|2.1))|(Windows Phone(OS 7|8.0))|(XBLWP)|(ZuneWP)|(w(eb)?OSBrowser)|(webOS)|()
     ))
 }*/
-var b = "KA";
-console.log(b);
 /*Selecting a file*/
 /* Reading the file*/
